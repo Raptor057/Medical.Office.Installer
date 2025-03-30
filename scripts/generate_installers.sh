a26eb736ad3d4da642ae7a65edf9cf2f5110bda4
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2025-03-30 13:35:04 -0500

    Update script copying in generate_installers.sh to use find for dynamic file retrieval

--- scripts/generate_installers.sh
+++ scripts/generate_installers.sh
@@ -12,9 +12,14 @@ cmake -S . -B build
 
 # Copiar scripts generados
 echo "📦 Copiando scripts a $OUTPUT_DIR..."
-cp build/start_osx.sh $OUTPUT_DIR/start_osx.sh      # macOS
-cp build/start_linux.sh $OUTPUT_DIR/start_linux.sh  # Linux
-cp build/start.ps1 $OUTPUT_DIR/start_windows.ps1    # Windows
+# cp build/start_osx.sh $OUTPUT_DIR/start_osx.sh      # macOS
+# cp build/start_linux.sh $OUTPUT_DIR/start_linux.sh  # Linux
+# cp build/start.ps1 $OUTPUT_DIR/start_windows.ps1    # Windows
+
+# Copiar los scripts generados a la carpeta de salida
+find build -name "start_osx.sh" -exec cp {} $OUTPUT_DIR/start_osx.sh \;
+find build -name "start_linux.sh" -exec cp {} $OUTPUT_DIR/start_linux.sh \;
+find build -name "start.ps1" -exec cp {} $OUTPUT_DIR/start_windows.ps1 \;
 
 # Dar permisos de ejecución a los scripts shell
 chmod +x $OUTPUT_DIR/start_osx.sh $OUTPUT_DIR/start_linux.sh
