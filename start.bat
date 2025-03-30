6ab00776005d43009bc40d546a6635b57762efe9
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2025-03-30 05:54:54 -0500

    Update Docker configuration and health checks; remove startup scripts

--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File scripts\check_docker_windows.ps1
-docker-compose up -d
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  bash scripts/check_docker_macos.sh
-else
-  bash scripts/check_docker_linux.sh
-fi
-
-docker-compose up -d
