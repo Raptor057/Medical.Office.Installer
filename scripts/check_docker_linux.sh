add1c6f7bd132ffa88a72b97595d23e73f4e94b1
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2025-03-30 13:53:51 -0500

    Remove obsolete CMake and Docker configuration files and scripts

--- scripts/check_docker_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if ! command -v docker &> /dev/null; then
-  echo "Docker no está instalado. Instalando..."
-  curl -fsSL https://get.docker.com | sh
-else
-  echo "Docker ya está instalado."
-fi
