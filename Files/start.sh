23b9466af5eba7b84b6d8341e9a97ee574b375a7
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2025-04-13 05:25:49 -0500

    chore: simplify start.sh by removing frontend build logic and improving echo messages

--- Files/start.sh
+++ Files/start.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 set -e
 
-API_URL="http://192.168.100.24:8080"
-
 #echo "✅ Usando variables de entorno desde .env.secure"
 #cp .env.secure .env
 
-echo "🔧 Construyendo imagen del frontend con NEXT_PUBLIC_API_URL=${API_URL}"
-docker build \
-  -t medicalofficefrontend \
-  --build-arg NEXT_PUBLIC_API_URL=${API_URL} \
-  -f ./Medical.Office.ReactWebClient/Dockerfile \
-  ./Medical.Office.ReactWebClient
 
-echo "🚀 Buscando actualizaciones de imágenes con Docker Compose Pull"
-docker compose pull || true
+echo "🚀 Buscando actualicacion de imagenes Docker Compose Pull"
+docker compose pull
 
-echo "🚀 Reconstruyendo servicios (excepto frontend ya compilado manualmente)"
-docker compose build || true
+echo "🚀 Buscando actualicacion de imagenes Docker Compose Build"
+docker compose build
 
-echo "🧹 Limpiando contenedores y volúmenes Docker Compose"
+echo "🚀 Limpiando contenedores y volúmenes Docker Compose"
 docker compose down
 
 echo "🚀 Levantando servicios con Docker Compose"
