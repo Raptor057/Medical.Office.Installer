f1e0f5292a8aca14d61bc81d9025cfb1ac7058f6
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2025-04-13 04:13:48 -0500

    chore: modify start.sh to clean up Docker containers and volumes without removing volumes

--- Files/start.sh
+++ Files/start.sh
@@ -4,14 +4,15 @@ set -e
 #echo "✅ Usando variables de entorno desde .env.secure"
 #cp .env.secure .env
 
-echo "🚀 Limpiando contenedores y volúmenes Docker Compose"
-docker compose down -v
 
 echo "🚀 Buscando actualicacion de imagenes Docker Compose Pull"
 docker compose pull
 
 echo "🚀 Buscando actualicacion de imagenes Docker Compose Build"
 docker compose build
+
+echo "🚀 Limpiando contenedores y volúmenes Docker Compose"
+docker compose down
 
 echo "🚀 Levantando servicios con Docker Compose"
 docker compose up -d
